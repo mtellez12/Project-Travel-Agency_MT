0ea50770304e2a82fb0c89c70dbe3dba262039a1
mac <mtellezfernandez6962@conestogac.on.ca>
2022-03-26 22:04:00 -0400

    Menu created

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ while start:
     print("************************************************")
     print("**************LOGIN MODULE**********************")
     print("************************************************\n")
-    print("\n***************ENTER INFORMATION**************\n (1) Create User \n", "(2) User Registered\n",
-          "(3) Quit Application\n")
+    print("\n***************ENTER INFORMATION**************\n (1) Create User \n", "(2) Manage Places\n",
+          "(3) Booking\n","(4) Reports \n", " (5) Quit Application\n")
 
     codes1 = []
     codes2 = []
@@ -51,10 +51,10 @@ while start:
 
         #Create connection with DB, and insert in to the table
 
-        connection = sqlite3.connect("USER.db")
+        connection = sqlite3.connect("Travel_Agency.db")
         # --------------------------------------------------------------------------------
         cursor = connection.cursor()
-        cursor.execute("INSERT INTO TB_USER (LOGIN, CRYPTOGRAPHIC_PASSWORD) values ('"+email+"','"+newpassword+"');")
+        cursor.execute("INSERT INTO Travel_Agency (LOGIN, CRYPTOGRAPHIC_PASSWORD) values ('"+email+"','"+newpassword+"');")
         cursor.execute("COMMIT;")
         cursor.close()
         # --------------------------------------------------------------------------------
@@ -64,7 +64,7 @@ while start:
     elif option == 2:
             count = ''
             print("\n**********************************************")
-            print("*******************USER REGISTERED**************")
+            print("*************MANAGE PLACES**********************")
             print("************************************************")
 
             email = input("Enter your email: \n")
@@ -87,10 +87,10 @@ while start:
                     if password[i] == codes1[j]:
                         newpassword = newpassword + codes2[j]
 
-            connection = sqlite3.connect("USER.db")
+            connection = sqlite3.connect("Travel_Agency.db")
             # --------------------------------------------------------------------------------
             cursor = connection.cursor()
-            cursor.execute("SELECT ACCESS_COUNT FROM TB_USER WHERE LOGIN = '"+email+"' AND CRYPTOGRAPHIC_PASSWORD = '"+newpassword+"'")
+            cursor.execute("SELECT ACCESS_COUNT FROM TRAVEL_AGENCY WHERE LOGIN = '"+email+"' AND CRYPTOGRAPHIC_PASSWORD = '"+newpassword+"'")
             count = cursor.fetchone()
             #access = type(int(count[0]))+ 1
             if(count != None):
@@ -99,45 +99,101 @@ while start:
                     access = access + 1
                 cursor.close()
                 # --------------------------------------------------------------------------------
+
             # update times to access in database
-                cursor = connection.cursor()
-                cursor.execute(
-                    "UPDATE TB_USER SET ACCESS_COUNT = "+str(access)+" WHERE LOGIN = '"+email+"' AND CRYPTOGRAPHIC_PASSWORD = '"+newpassword+"';")
-                cursor.execute("COMMIT;")
-                cursor.close()
+               # cursor = connection.cursor()
+                #cursor.execute(
+                 #   "UPDATE TB_USER SET ACCESS_COUNT = "+str(access)+" WHERE LOGIN = '"+email+"' AND CRYPTOGRAPHIC_PASSWORD = '"+newpassword+"';")
+                # cursor.execute("COMMIT;")
+                # cursor.close()
             # Qry User and Nro times to access
                 #---------------------------------------------------------------------------------------------
-                cursor = connection.cursor()
-                cursor.execute(
-                    "SELECT LOGIN, ACCESS_COUNT FROM TB_USER WHERE LOGIN = '" + email + "' AND CRYPTOGRAPHIC_PASSWORD = '" + newpassword + "'")
-                results1 = cursor.fetchall()
-                cursor.close()
+                # cursor = connection.cursor()
+                # cursor.execute(
+                  #  "SELECT LOGIN, ACCESS_COUNT FROM TB_USER WHERE LOGIN = '" + email + "' AND CRYPTOGRAPHIC_PASSWORD = '" + newpassword + "'")
+                # results1 = cursor.fetchall()
+                # cursor.close()
 
             # Qry all Data to backup file
                 #-------------------------------------------------------------------------------------------------------------
-                cursor = connection.cursor()
-                cursor.execute(
-                    "SELECT * FROM TB_USER ")
-                results2 = cursor.fetchall()
-                cursor.close()
+               # cursor = connection.cursor()
+                #cursor.execute(
+                 #   "SELECT * FROM TB_USER ")
+                #results2 = cursor.fetchall()
+                # cursor.close()
 
-                for i in range(len(results1)):
-                    print("\nSuccessful Access, Your User and Login Number is:"+ str(results1[i])+"\n")
+                # for i in range(len(results1)):
+                  #  print("\nSuccessful Access, Your User and Login Number is:"+ str(results1[i])+"\n")
                 # --------------------------------------------------------------------------------
-                connection.close()
+                #connection.close()
             # Write backup file
-                file_backup = open('userdb-backup.csv', mode='w')
+                #file_backup = open('userdb-backup.csv', mode='w')
 
                 # Write file operation
-                for i in range(len(results2)):
-                    file_backup.write(str(results2[i])+"\n")
+                #for i in range(len(results2)):
+                    #file_backup.write(str(results2[i])+"\n")
 
-                file_backup.close()
+                #file_backup.close()
 
             else:
                 print("\n The user or password is not correct try again, please \n")
 
     elif option == 3:
+            count = ''
+            print("\n**********************************************")
+            print("*******************BOOKING**********************")
+            print("************************************************")
+
+            email = input("Enter your email: \n")
+            email = email.upper()
+            password = input("Enter your password: \n")
+            password = password.upper()
+
+            with open('./chyper-code.csv') as file:
+                my_file = csv.reader(file)
+
+                for row in my_file:
+                    key = row[0]  # store row of the file
+                    x = key.split(";")  # used to separate the string
+                    codes1.append(x[0])
+                    codes2.append(x[1])
+
+            # Password encription
+            for i in range(len(password)):
+                for j in range(len(codes1)):
+                    if password[i] == codes1[j]:
+                        newpassword = newpassword + codes2[j]
+
+
+    elif option == 4:
+            count = ''
+            print("\n**********************************************")
+            print("*******************REPORTS**********************")
+            print("************************************************")
+
+            email = input("Enter your email: \n")
+            email = email.upper()
+            password = input("Enter your password: \n")
+            password = password.upper()
+
+            with open('./chyper-code.csv') as file:
+                my_file = csv.reader(file)
+
+                for row in my_file:
+                    key = row[0]  # store row of the file
+                    x = key.split(";")  # used to separate the string
+                    codes1.append(x[0])
+                    codes2.append(x[1])
+
+            # Password encription
+            for i in range(len(password)):
+                for j in range(len(codes1)):
+                    if password[i] == codes1[j]:
+                        newpassword = newpassword + codes2[j]
+
+
+
+    elif option == 5:
         start = False
 
 
